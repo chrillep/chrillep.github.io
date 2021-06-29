25202794369d2cef83f41f81944f09f57e1cbd55
Christian Widlund <christian.widlund@gmail.com>
2021-06-29 20:04:02 +0200

    reset

--- components/date-formatter.tsx
+++
@@ -1,7 +0,0 @@
-import { format, parseISO } from 'date-fns'
-import React from 'react'
-
-export default function DateFormatter({ dateString }) {
-  const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>
-}
