70c6f26fc550fda9f86bee576067ef0739664e3b
Christian Widlund <christian.widlund@gmail.com>
2023-02-26 21:16:30 +0100

    ok, ok ill turn it off

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -4,7 +4,6 @@ module.exports = {
     "next",
     "next/core-web-vitals",
     "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "prettier",
     "turbo",
   ],
