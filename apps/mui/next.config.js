3b8eb20e7c7b325aee8516086479b7f597f72d3a
Christian Widlund <christian.widlund@gmail.com>
2023-02-26 22:15:01 +0100

    lock it down

--- apps/mui/next.config.js
+++ apps/mui/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
     reactStrictMode: true,
+    transpilePackages: ["ui"],
     images: {
         domains: ['www.gravatar.com'],
     },
